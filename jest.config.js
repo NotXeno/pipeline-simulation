74829f031578dfef66bb82a5308c3e13d87c5812
NotXeno <alvino.wilbert.ho@gmail.com>
2025-10-23 11:04:19 +0700

    change branches to 90 for config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      branches: 100,
+      branches: 90,
       functions: 100,
       lines: 100,
       statements: 100
